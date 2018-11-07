441233ddf969587c8a2ecc9a9e6f683cca835ab3
Georgiy Sgibnev <georgiy@lab50.net>
2018-11-07 16:13:35 +0300

    Hot fix to use napa modules

--- spiceproxy/requirements.js
+++ spiceproxy/requirements.js
@@ -4,15 +4,15 @@ window.jQuery = jQuery;
 require("jquery-mousewheel");
 require("jquery.nok");
 
-var Pixastic = require("exports-loader?Pixastic!pixastic_178dd7313b/pixastic.core.js");
+var Pixastic = require("exports-loader?Pixastic!../node_modules/pixastic_178dd7313b/pixastic.core.js");
 window.Pixastic = Pixastic;
-require("pixastic_178dd7313b/pixastic.jquery.js");
-require("pixastic_178dd7313b/actions/hsl.js");
-require("pixastic_178dd7313b/actions/invert.js");
+require("../node_modules/pixastic_178dd7313b/pixastic.jquery.js");
+require("../node_modules/pixastic_178dd7313b/actions/hsl.js");
+require("../node_modules/pixastic_178dd7313b/actions/invert.js");
 
-var bowser = require("bowser_1_1_0/src/bowser.js");
+var bowser = require("../node_modules/bowser_1_1_0/src/bowser.js");
 window.bowser = bowser;
 
 var BigInteger = require("biginteger").BigInteger;
 var SecureRandom = require("jsbn").SecureRandom;
-var prettyPrint = require("exports-loader?prettyPrint!prettyprint/prettyprint.js");
+var prettyPrint = require("exports-loader?prettyPrint!../node_modules/prettyprint/prettyprint.js");
