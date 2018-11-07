07e035946022cdb4ff7045d822a887f551bca84e
Georgiy Sgibnev <georgiy@lab50.net>
2018-11-07 11:48:34 +0300

    More export functions

--- spiceproxy/exports.js
+++ spiceproxy/exports.js
@@ -1,4 +1,12 @@
 // The end of the result js file.
 exports.wdi = wdi;
 exports.translations = translations;
+exports.closeSession = closeSession;
+
 exports.setInactivityTimer = setInactivityTimer;
+exports.showMenuBar = showMenuBar;
+exports.hideMenuBar = hideMenuBar;
+exports.toggleFullScreen = toggleFullScreen;
+exports.sendCtrlAltDel = sendCtrlAltDel;
+exports.showCloseDialog = showCloseDialog;
+exports.closeAction = closeAction;
